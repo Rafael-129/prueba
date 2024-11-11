1655eca89eaa2b3e19580104739563fa7186c0ba
Wilton Sanchez <martinsanchez3453@gmail.com>
2024-11-11 12:46:34 -0500

    cambio en anunciosProfs

--- app/Http/Controllers/AnunciosProfController.php
+++ app/Http/Controllers/AnunciosProfController.php
@@ -13,7 +13,7 @@ class AnunciosProfController extends Controller
     public function index()
     {
         $anuncios_profs = AnunciosProf::all();
-        return view('Anuncios.index',compact('anuncios'));
+        return view('Anuncios.index',compact('anuncios_profs'));
        
     }
 
