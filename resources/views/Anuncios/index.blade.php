ae3b0b248c3f4d07a9f88810177f36b7817e6ee5
Wilton Sanchez <martinsanchez3453@gmail.com>
2024-11-12 15:41:22 -0500

    cambio en idex,blade

--- resources/views/Anuncios/index.blade.php
+++ resources/views/Anuncios/index.blade.php
@@ -3,5 +3,36 @@
     -Listado
 @endsection
 @section('body')
-    Index
+    <div class="row">
+        <div class="col-12">
+            <div class="table-responsive">
+                <table class="table table-bordered table-hover">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>IMAGEN</th>
+                            <th>F. PUBLICACIÓN</th>
+                            <th>F. EVENTO</th>
+                            <th>LUGAR</th>
+                            <th>DETALLE</th>
+                            <th></th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach($games as $i => $row)
+                        <tr>
+                            <td>{{$i}}</td>
+                            <td>{{$row->image}}</td>
+                            <td>{{$row->fechapub}}</td>
+                            <td>{{$row->fechaev}}</td>
+                            <td>{{$row->lugar}}</td>
+                            <td>{{$row->detalle}}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+
 @endsection
