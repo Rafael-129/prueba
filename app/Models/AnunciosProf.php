14a786fcacb7a5ff8e66d30cb3956a94dc98b4c2
Wilton Sanchez <martinsanchez3453@gmail.com>
2024-11-13 15:08:42 -0500

    cambio anuncios

--- app/Models/AnunciosProf.php
+++ app/Models/AnunciosProf.php
@@ -8,14 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class AnunciosProf extends Model
 {
     use HasFactory;
-
-    protected $table = 'anuncios_profs'; // Verifica si coincide con el nombre de la tabla
-    protected $primaryKey = 'id'; // Verifica si coincide con la llave primaria
-    protected $fillable = ['image', 'fechapub', 'fechaev', 'lugar', 'detalle', 'created_at', 'updated_at'];
-    protected $casts = [
-        'fechapub' => 'datetime',
-        'fechaev' => 'datetime',
-        'created_at' => 'datetime',
-        'updated_at' => 'datetime',
-    ];
+    protected $fillable = ['image', 'fechapub', 'fechaev', 'lugar', 'detalle'];
+    
 }
