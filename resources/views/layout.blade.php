d9f74a9727bfba553a0f20a45ec2ef1347b2bd09
Wilton Sanchez <martinsanchez3453@gmail.com>
2024-11-17 17:04:11 -0500

    anuncio

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,3 +1,9 @@
+@extends('layouts.headerIntra') <!-- Asegúrate de usar el nombre correcto del archivo de layout -->
+
+@section('title', 'Contactos')
+
+@section('content')
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,29 +11,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Anuncios @yield('title')</title>
-    @vite(['resources/css/app.css','resources/js/app.js'])
+ 
 </head>
 <body>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <div class="container-fluid">
-          <a class="navbar-brand">Anuncios</a>
-          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-          <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-              <li class="nav-item">
-                <a class="nav-link active" aria-current="page" href="{{url('anuncios_profs')}}">Listado</a>
-              </li>
-              <li class="nav-item">
-                <a class="nav-link" href="{{url('anuncios_profs/create')}}">Crear</a>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </nav>
+
       <div class="container mt-3">
         @yield('body')
       </div>
 </body> 
 </html>
+
+
+
+@endsection
